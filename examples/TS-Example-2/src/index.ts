83557d6505fd8e4ef8193e30ac4c27d19d812c58
Julian Medina <julian.medina@truepill.com>
2022-02-25 22:48:18 -0500

    designs

--- examples/TS-Example-2/src/index.ts
+++ examples/TS-Example-2/src/index.ts
@@ -4,12 +4,6 @@ import Skeleton, { FolderSkeleton } from "skeleton-code-generator";
 const folderToGenerate = path.join(__dirname, "generationExample");
 const folderSkeleton: FolderSkeleton = {
     name: 'RootFolder',
-    files: [
-        {
-            name: "File 0-1.txt",
-            content: "File 0-1 text content"
-        },
-    ],
     subfolders: [
         {
             name: 'ChildFolder1',
@@ -23,6 +17,51 @@ const folderSkeleton: FolderSkeleton = {
                     content: "File 1-2 text content"
                 }
             ],
+        },
+        {
+            name: 'ChildFolder2',
+            files: [
+                {
+                    name: "File 2-1.txt",
+                    content: "File 2- 1 text content"
+                },
+                {
+                    name: "File 2-2.txt",
+                    content: "File 2-2 text content"
+                },
+                {
+                    name: "File 2-3.txt",
+                    content: "File 2-3 text content"
+                },
+                {
+                    name: "File 2-4.txt",
+                    content: "File 2-4 text content"
+                }
+            ],
+        },
+        {
+            name: 'ChildFolder3',
+            files: [
+                {
+                    name: "File 3-1.txt",
+                    content: "File 3- 1 text content"
+                }
+            ],
+            subfolders: [
+                {
+                    name: 'ChildFolder31',
+                    files: [
+                        {
+                            name: "File 31-1.txt",
+                            content: "File 31-1 text content"
+                        },
+                        {
+                            name: "File 31-2.txt",
+                            content: "File 31-2 text content"
+                        }
+                    ],
+                }
+            ]
         }
     ]
 }
